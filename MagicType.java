91f292da8d87e8c4684ca963fda64ca61caf5d61
Justin Riley <20hz20khz@gmail.com>
2023-10-16 21:19:51 -0400

    Create MagicType.java

---
+++ MagicType.java
@@ -0,0 +1,19 @@
+public class MagicType
+{
+  // class variables
+  /** 
+   * Represents fire magic
+   */ 
+  public static final int FIRE = 1;
+
+  // class methods
+  /**
+   * Returns a randomly selected MagicType
+   * @return a randomly selected MagicType
+   */
+  public static MagicType getRandomMagicType()
+  {
+    int dice = (int)(Math.random());
+  }
+
+}
